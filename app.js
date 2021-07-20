9575cbb94201154f3e4e7bed196cd56669664c79
Victor Carpenter <victorcarpenter123@gmail.com>
2021-07-20 15:23:01 -0400

    save

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
 var canvas = document.querySelector("canvas");
 var game = canvas.getContext("2d");
-var x = 0;
-var y = 0;
-var xspeed = 0;
-var yspeed = 0;
+
+
+var character = {
+  health:75,
+  x:0,
+  y:0,
+  attack:10,
+  defense:5,
+  xspeed:0,
+  yspeed:0,
+  status:'poisoned',
+  height:50,
+  width:20,
+  attacking: false,
+  attackHit: false,
+  jumpCount:0,
+  specialMoves:[
+    {
+      name:'fireball',
+      attack:20,
+      type:'magic',
+      element:'fire'
+    }
+  ],
+  weapon:{
+    x:0,
+    y:0
+  }
+}
+
+var enemy = {
+  health:50,
+  x:200,
+  y:0,
+  attack:5,
+  defense:0,
+  xspeed:0,
+  yspeed:0,
+  height:50,
+  width:20,
+  jumpCount:0
+}
+
+
+
 var gravity = 1;
-var height = 50;
-var ground = canvas.height - height;
-var jumpCount = 0;
+var ground = canvas.height - character.height;
 var level = 0;
 var itemCount = 15;
 var gameState = true;
@@ -45,6 +84,8 @@ var playerDeath = new Image();
 playerDeath.src = "https://raw.githubusercontent.com/mikeplott/TOAdventure/master/public/avatars/elf-death.png"
 
 var playerImage = playerRight;
+
+var enemyImage = playerRight;
 
 var fullHealth = new Image();
 fullHealth.src = "https://raw.githubusercontent.com/mikeplott/TOAdventure/master/public/ui/full-heart.png"
@@ -88,28 +129,36 @@ function draw() {
   game.drawImage(background,0,0,canvas.width,canvas.height);
   game.font = "10px Arial";
   game.fillStyle = 'white';
-  game.fillText("Level: " + level, 10, 10);
-  game.fillText("Score: " + score, 10, 20);
-  x = x + xspeed;
-  y = y + yspeed;
-  yspeed = yspeed + gravity;
+  game.fillText("Enemy Health: " + enemy.health, 10, 10);
+  game.fillText("Character Health: " + character.health, 10, 20);
+  game.fillText("Character active status: " + character.status, 10, 30);
+  handleCollisions()
+  character.x = character.x + character.xspeed;
+  character.y = character.y + character.yspeed;
+  character.yspeed = character.yspeed + gravity;
+  character.weapon.x = character.x + character.width
+  character.weapon.y = character.y + (character.height/2)
 
-  if (y > ground) {
-    y = ground;
-    yspeed = 0;
-    jumpCount = 0;
+  if (character.y > ground) {
+    character.y = ground;
+    character.yspeed = 0;
+    character.jumpCount = 0;
     playerImage = playerRight
   }
-  if (x < 0){
-    x = 0;
+
+  if (character.y < ground){
+    playerImage = playerJump
   }
 
-  if (y < ground){
-     playerImage = playerJump
- }
+  if (character.x < 0){
+    character.x = 0;
+  }
 
-  if (x > canvas.width - 30){
-    x = canvas.width - 30;
+  if (character.x > canvas.width - 30){
+    character.x = canvas.width - 30;
+  }
+   if(score <= 0){
+    score = 0;
   }
 
  if(health === 3){
@@ -133,56 +182,48 @@ function draw() {
    endGame();
  }
 
+ switch(character.status){
+   case 'poisoned':
+     character.health = character.health - 0.02
+     break;
+ }
+
+ if(enemy.health <= 0){
+  endGame();
+ }
+
+ game.fillStyle = "#ddd";
+ game.fillRect(character.weapon.x, character.weapon.y, 30, 3);
+
+
+  game.drawImage(playerImage, character.x, character.y, character.height * imageRatio, character.height);
 
 
 
-  if (obstacleArray.length === 0){
+  enemy.x = enemy.x + enemy.xspeed;
+  enemy.y = enemy.y + enemy.yspeed;
+  enemy.yspeed = enemy.yspeed + gravity;
 
-    itemCount = itemCount + 5
-    for (i = 0; i < itemCount ; i++){
-
-      obstacleArray.push({
-        x: 0,
-        catName: "item",
-        y: 0
-     },
-     {
-      catName: "money",
-      x: 0,
-      y: 0
-   },
-   {
-     catName: "enemy",
-     x: 0,
-     y: 0
-  }
-   // {
-   //   catName: "health",
-   //   x: 903,
-   //   y: theY3
-   // }
-   )
-    }
+  if (enemy.y > ground) {
+    enemy.y = ground;
+    enemy.yspeed = 0;
+    enemy.jumpCount = 0;
+    enemyImage = playerRight
   }
 
-  if(score <= 0){
-    score = 0;
+  if (enemy.y < ground){
+    enemyImage = playerJump
   }
 
-  displayedObjects.forEach(function(item) {
-    item.draw();
-  });
-  displayedObjects.forEach(function(item) {
-    item.update();
-  });
-  displayedObjects = displayedObjects.filter(function(item) {
-    return item.active;
-  });
-
-  handleCollisions();
+  game.drawImage(enemyImage, enemy.x, enemy.y, enemy.height * imageRatio, enemy.height);
 
 
-  game.drawImage(playerImage, x, y, height * imageRatio, height);
+
+
+  if(character.attacking &&  !character.attackHit){
+    enemy.health = enemy.health - character.attack
+    character.attackHit = true
+  }
 }
 
 var gameUpdate = setInterval(draw, 20);
@@ -190,215 +231,112 @@ var gameUpdate = setInterval(draw, 20);
 var endGame = function(){
    backgroundMusic.pause()
   game.drawImage(gameEnd, 5,30,300,100)
-  clearInterval(objectCycle);
+  // clearInterval(objectCycle);
   clearInterval(gameUpdate);
   playerHurtSound.play()
   setTimeout(function(){
      playerHurtSound.play()
      gameOverSound.play()
-    y += -15;
+    enemy.y += -15;
     var fallingDeath = setInterval(function(){
 
-    playerImage = playerDeath;  game.drawImage(background,0,0,canvas.width,canvas.height);
+    enemyImage = playerDeath;  game.drawImage(background,0,0,canvas.width,canvas.height);
   game.font = "10px Arial";
   game.fillStyle = 'white';
   game.fillText("Level: " + level, 10, 10);
   game.fillText("Score: " + score, 10, 20);
-    x = x;
-    y = y + yspeed;
-    yspeed = yspeed + gravity;
-    game.drawImage(playerImage, x, y, height * imageRatio, height)
+  enemy.x = enemy.x;
+  enemy.y = enemy.y + enemy.yspeed;
+  enemy.yspeed = enemy.yspeed + gravity;
+    game.drawImage(enemyImage, enemy.x, enemy.y, enemy.height * imageRatio, enemy.height)
     game.drawImage(gameEnd, 5,30,300,100)
-    if(y > ground){
-      y = ground;
-      yspeed = 0;
+    if(enemy.y > ground){
+      enemy.y = ground;
+      enemy.yspeed = 0;
       playerSplatSound.play()
       clearInterval(fallingDeath)
     }
 },45)
-
-
-
   }, 800)
 }
 
 function startMove(event) {
   // pressed left
   if (event.keyCode == 37) {
-    xspeed = -5;
+    character.xspeed = -5;
     playerImage = playerLeft;
   }
   // pressed right
   if (event.keyCode == 39) {
-    xspeed = 5;
+    character.xspeed = 5;
     playerImage = playerRight;
   }
   // pressed up
   if (event.keyCode == 38) {
-    if (jumpCount < 2){
+    if (character.jumpCount < 2){
       playerJumpSound.load()
       playerJumpSound.play()
-        yspeed = -10;
-        jumpCount += 1;
+        character.yspeed = -10;
+        character.jumpCount += 1;
         playerImage = playerJump;
         }
   }
-}
-
-
-
-var obstacle = function (O) {
-
-   O.active = true;
-   O.item = new Image()
-
-   if(O.name === 'money'){
-      O.item.src ="https://raw.githubusercontent.com/mikeplott/TOAdventure/master/public/money/money.png"
-   } else if (O.name === 'item'){
-      O.item.src ="https://raw.githubusercontent.com/mikeplott/TOAdventure/master/public/items/trident.png"
-   } else if (O.name === 'enemy'){
-      O.item.src = "https://raw.githubusercontent.com/mikeplott/TOAdventure/master/public/npcs/enemy1.png"
-   } else if (O.name === 'health'){
-      O.item.src = "https://raw.githubusercontent.com/mikeplott/TOAdventure/master/public/ui/full-heart.png"
-   }
-
-  O.xVelocity = 0;
-  O.yVelocity = 0;
-  O.width = 20;
-  O.height = 20;
-  O.color = "#FFF";
-
-  O.inBounds = function() {
-    if(O.y >= 0){
-      return false
-    }
+  // pressed spacebar
+  if(event.keyCode == 32){
+    attack()
   }
-
-  O.draw = function() {
-    game.drawImage(this.item,this.x, this.y, this.width, this.height)
-    // game.fillStyle = this.color;
-    // game.fillRect(this.x, this.y, this.width, this.height);
-  };
-
-  O.update = function() {
-    O.x += -3;
-    O.y += 0;
-
-    if(O.x === 120){
-      if(O.name === "enemy"){
-         enemySound.play()
-      }
-   }
-
-    if(O.x <= 0){
-      score += 1;
-      O.active = false
-
-    } else {
-      O.active = O.active
-    }
-
-
-  };
-
-  return O;
-
 }
 
+document.onkeydown = startMove;
 
-
-var objectThrowing = function(){
-   var itemTracks = [25,50,75,100,125]
-   var xTracks = [900,920,950]
-  if(Date.now() > startTime){
-
-   if(displayedObjects.length === 0){
-    level += 1;
+function stopMove(event) {
+  if (event.keyCode == 37 || event.keyCode == 39) {
+    character.xspeed = 0;
   }
+}
 
-  var objAmount = (Math.floor(Math.random() * 3) + 1)
-  if (objAmount > obstacleArray.length){
-     objAmount = obstacleArray.length
- }
+document.onkeyup = stopMove;
 
-   for(var b = 0; b < objAmount; b++ ){
-      var crntXIndex = (Math.floor(Math.random() * xTracks.length))
-      var crntIndex = (Math.floor(Math.random() * itemTracks.length))
-      var crntY = itemTracks[crntIndex]
-      var crntX = xTracks[crntXIndex]
-      var crntItem = obstacleArray[0]
-      displayedObjects.push(obstacle({
-        speed: -10,
-        name: crntItem.catName,
-        x: crntX,
-        y: crntY
-      }));
-      itemTracks.splice(crntIndex, 1)
-     obstacleArray.splice(0,1)
-   }
 
-  if(obstacleArray.length === 0){
-
-    startTime = Date.now() + 10000
+var attack = function(){
+  if(!character.attacking){
+    character.attacking = true
+    setTimeout(function(){
+      character.attacking = false
+      character.attackHit = false
+    },1000)
+   
   }
-  console.log(obstacleArray.length)
 }
 
 
-}
+var collision = function (enemy, me){
 
-var objectCycle = setInterval(objectThrowing, 900)
-
-var collision = function (item, me){
-
-  if(item.x < x + height * imageRatio &&
-     item.x + item.width > x &&
-     item.y < y + height &&
-     item.y + item.height > y){
+  if(enemy.x < me.x + me.height * imageRatio &&
+     enemy.x + enemy.width > me.x &&
+     enemy.y < me.y + me.height &&
+     enemy.y + enemy.height > me.y){
     return true
   }
 
 }
 
 var handleCollisions = function (){
-  displayedObjects.forEach(function(item){
-    if(collision(item)){
-      if(item.name === "enemy"){
-         item.active = false
-         score += -5;
-         if(health > 0){
-           health += -1;
-           playerHurtSound.play()
-
-         }
-      } else if (item.name === "item"){
-         itemSound.play()
-         item.active = false
-         score += 5;
-      } else if (item.name === "money"){
-         moneySound.play()
-         item.active = false
-         score += 10;
-      } else if (item.name === "health"){
-         item.active = false
-         itemSound.play()
-         health += 1;
-         if(health > 3){
-            health = 3;
-         }
-      }
-
+  if(collision(enemy,character)){
+    if(character.xspeed > 0){
+      character.xspeed = 0
     }
-  })
-}
-
-
-document.onkeydown = startMove;
-
-function stopMove(event) {
-  if (event.keyCode == 37 || event.keyCode == 39) {
-    xspeed = 0;
   }
 }
 
-document.onkeyup = stopMove;
+
+var handleAttackCollision = function (){
+  if(collision(enemy,character.weapon)){
+    if(!character.attackHit){
+      enemy.health = enemy.health - character.attack
+     
+    }
+  }
+}
+
+
